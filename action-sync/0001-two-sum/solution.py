d2d1403149ba156570299020900ac87e27754946
GitHub Action <action@github.com>
2024-07-11 21:00:28 +0000

    [LeetCode Sync] Runtime - 59 ms (32.14%), Memory - 17.8 MB (100.00%)

--- action-sync/0001-two-sum/solution.py
+++ action-sync/0001-two-sum/solution.py
@@ -1,11 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
         n = len(nums)
+        map = {}
 
         for i in range(n):
-            com = target-nums[i]
+            com = target - nums[i]
             if com in map:
-                return [map[com], i]
+                return [map[com],i]
             map[nums[i]] = i
         return []
+
