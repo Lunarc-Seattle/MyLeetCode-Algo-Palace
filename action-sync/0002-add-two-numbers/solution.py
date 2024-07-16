aec7c775afce77e67595cf048cb0073b4b78e97b
GitHub Action <action@github.com>
2024-07-16 07:01:03 +0000

    [LeetCode Sync] Runtime - 62 ms (6.99%), Memory - 16.6 MB (100.00%)

--- action-sync/0002-add-two-numbers/solution.py
+++ action-sync/0002-add-two-numbers/solution.py
@@ -5,37 +5,36 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummyHead = ListNode(0)
+        currentHead = dummyHead
         carry = 0
-        dummy_head = ListNode(0)
-        current =dummy_head
 
         while l1 is not None or l2 is not None:
             if l1 is not None:
                 x = l1.val
-            else: x=0 
+            else: x = 0
+
             if l2 is not None:
                 y = l2.val
-            else: y = 0
+            else: y= 0
 
-            sum = x + y + carry
-            digit = sum % 10
-            carry = sum //10
+            digit = (x+y+carry)%10
+            carry = (x+y+carry)//10
 
             newNode = ListNode(digit)
-            current.next = newNode
-            current = current.next
+            currentHead.next = newNode
+            currentHead = currentHead.next
 
             if l1 is not None:
                 l1 = l1.next
             
             if l2 is not None:
                 l2 = l2.next
-               
-            
-        result = dummy_head.next
+                
+
+            result = dummyHead.next
+
         if carry>0:
-            current.next = ListNode(carry)
+            currentHead.next = ListNode(carry)
         return result
-
-            
-
