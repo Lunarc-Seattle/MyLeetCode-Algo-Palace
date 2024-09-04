f7686bd7c9d5f6f8c37b59c233d1e821fd0e116e
GitHub Action <action@github.com>
2024-09-04 21:57:31 +0000

    [LeetCode Sync] Runtime - 38 ms (4.15%), Memory - 16.4 MB (100.00%)

---
+++ action-sync/0020-valid-parentheses/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for c in s:
+            if c == '{' or c =='[' or c =='(':
+                stack.append(c)
+            else:
+                if len(stack) == 0:
+                    return False
+                temp = stack.pop()
+                if c == '}':
+                    if temp != '{':
+                        return False   
+                if c == ')':
+                    if temp != '(':
+                        return False
+                if c == ']':
+                    if temp != '[':
+                         return False
+        return len(stack) == 0
+                        
+
+
+        
