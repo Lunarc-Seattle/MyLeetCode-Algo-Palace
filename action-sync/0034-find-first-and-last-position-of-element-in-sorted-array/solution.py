a0a6e0d85f10ac76528694ac642c94b996e62722
GitHub Action <action@github.com>
2024-12-11 04:51:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.4 MB (100.00%)

--- action-sync/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ action-sync/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,42 +1,42 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def findFirst(nums, target):
-            left, right = 0, len(nums)-1
-            while left<= right:
-                mid = (left+right)//2
-                if nums[mid]< target:
-                    left = mid+1
-                elif nums[mid]> target:
-                    right = mid-1
-                else:  
-                    if mid ==0 or nums[mid-1]!= target:
+        def findFirst( nums, target):
+            l = 0
+            r = len(nums)-1
+            while l <= r :
+                mid = (l+r)//2
+                if nums[mid] < target:
+                    l = mid + 1
+                elif nums[mid] > target:
+                    r = mid -1
+                else:
+                    if mid == 0 or nums[mid-1] != target:
                         return mid
-                    right = mid -1
+                    r = mid -1
             return -1
         
-        def findLast(nums, target):
-            left,right = 0,len(nums)-1
-            while left<= right:
-                mid = (left+right)//2
+
+        def findLast( nums, target ):
+            l, r = 0, len(nums)-1
+            while l <= r:
+                mid = (l+r)//2
                 if nums[mid] < target:
-                    left =mid+1
-                elif nums[mid]> target:
-                    right = mid -1
-                else:
+                    l = mid + 1
+                elif nums[mid] > target:
+                    r = mid - 1
+                else: 
                     if mid == len(nums)-1 or nums[mid+1] != target:
                         return mid
-                    left = mid+1
+                    l = mid + 1
             return -1
+        start  = findFirst(nums, target)
+        last = findLast(nums, target)
+        return [start, last]
+
+
+
+
         
-
-        left = findFirst(nums, target)
-        right = findLast(nums, target)
-
-        return [left,right]
-                
-                
-       
-
 
 
         
