7ef9e5db69d7361496917680283c965f03aebba6
GitHub Action <action@github.com>
2024-12-14 21:30:16 +0000

    [LeetCode Sync] Runtime - 20 ms (14.00%), Memory - 48.1 MB (99.94%)

--- action-sync/0049-group-anagrams/solution.java
+++ action-sync/0049-group-anagrams/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, ArrayList<String>> ans = new HashMap<>();
-            for(String s: strs){
-                int count[] = new int[26];
-                for (char c : s.toCharArray() ){
-                    count[c - 'a']++;
+        HashMap<String, List<String>>ans = new HashMap<>();
+            for (String word : strs){
+                int [] count = new int[26];
+                for ( char c : word.toCharArray()){
+                    count[c-'a']++;
                 }
                 StringBuilder sb = new StringBuilder();
-                for (int e : count){
-                    sb.append(e).append("#");
-                }
+                for( int c : count ){
+                    sb.append(c).append("#");
+                } 
                 String key = sb.toString();
                 if(!ans.containsKey(key)){
-                    ans.put(key,new ArrayList<>());
+                    ans.put(key, new ArrayList<>());
                 }
-                ans.get(key).add(s);
-
-
+                ans.get(key).add(word);
             }
             return new ArrayList<>(ans.values());
         
