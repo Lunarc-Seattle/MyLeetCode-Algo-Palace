d0842fd815a3fd8cb5e57711f9179438b0c31b16
GitHub Action <action@github.com>
2024-12-19 05:58:47 +0000

    [LeetCode Sync] Runtime - 7 ms (94.34%), Memory - 28.3 MB (100.00%)

---
+++ action-sync/0209-minimum-size-subarray-sum/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        res = float("inf")
+        l = 0
+        sum = 0
+        for r in range(len(nums)):
+            sum += nums[r]
+
+            while sum >= target:
+                if r - l + 1 < res:
+                    res = r - l + 1
+                sum -= nums[l]
+                l += 1
+        return res if res != float("inf") else 0
+        
