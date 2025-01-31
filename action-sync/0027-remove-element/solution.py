9a59db1482f73557c27d111e7cf0332c443757bf
GitHub Action <action@github.com>
2025-01-31 03:14:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- action-sync/0027-remove-element/solution.py
+++ action-sync/0027-remove-element/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        if nums is None or len(nums) ==0:
-            return 0
-        res = 0
+        k = 0
         for i in range(len(nums)):
-            if nums[i] !=val:
-                nums[res] = nums[i]
-                res +=1
-        return res
+            if nums[i] != val:
+                nums [k] = nums[i]
+                k += 1
+        return k
         
