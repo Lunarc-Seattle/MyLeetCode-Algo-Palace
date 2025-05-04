9dec89ab3dd16cadfdc07d6446b286c5dfe1f14e
GitHub Action <action@github.com>
2025-05-04 13:05:58 +0000

    [LeetCode Sync] Runtime - 30 ms (99.54%), Memory - 17.8 MB (100.00%)

--- action-sync/1894-merge-strings-alternately/solution.py
+++ action-sync/1894-merge-strings-alternately/solution.py
@@ -1,23 +1,12 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        result = []
-        # use list rather than []
-        i,j =0,0
-        # needs 2 0
-
-        while i<len(word1) and j < len(word2): 
-        # while needs iterate and " : " 
-            result.append(word1[i])
-            result.append(word2[j])
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i,j = 0,0
+        res = []
+        while i<len(word1)and j<len(word2):
+            res.append(word1[i])
+            res.append(word2[j])
             i+=1
             j+=1
-
-        result.append(word1[i:])
-        result.append(word2[j:])
-        return ''.join(result)
-        # toString
+        res.append(word1[i:])
+        res.append(word2[j:])
+        return "".join(res)     
