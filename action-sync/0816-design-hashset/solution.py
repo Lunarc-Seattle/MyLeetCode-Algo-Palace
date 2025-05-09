c0a37d07a7b66a4a21272b826a0d35c95d61dea0
GitHub Action <action@github.com>
2025-05-09 23:49:40 +0000

    [LeetCode Sync] Runtime - 39 ms (60.08%), Memory - 22.8 MB (100.00%)

--- action-sync/0816-design-hashset/solution.py
+++ action-sync/0816-design-hashset/solution.py
@@ -1,46 +1,23 @@
-class ListNode:
-    def __init__(self, key: int):
-        self.key = key
-        self.next = None
 class MyHashSet:
 
     def __init__(self):
-        self.set = []
-        for i in range( 10**4 ):
-            self.set.append(ListNode(0))
-        
-        
+        self.size = 1009  # 用一个质数，减少冲突
+        self.buckets = [[] for _ in range(self.size)]
 
-    def add(self, value: int) -> None:
-        cur = self.set[value%len(self.set)]
-        while cur.next:
-            if cur.next.key == value:
-                return
-            cur = cur.next
-        cur.next = ListNode(value)
-        
+    def _hash(self, key):
+        return key % self.size  # 简单的哈希函数
 
-    def remove(self, value: int) -> None:
-        cur = self.set[value%len(self.set)]
-        while cur.next:
-            if cur.next.key == value:
-                cur.next = cur.next.next
-                return
-            cur = cur.next
-        
+    def add(self, key: int) -> None:
+        bucket = self.buckets[self._hash(key)]
+        if key not in bucket:  # 避免重复添加
+            bucket.append(key)
 
-    def contains(self, value: int) -> bool:
-        cur = self.set[value%len(self.set)]
-        while cur.next:
-            if cur.next.key == value:
-                return True
-            cur = cur.next
-        return False
-        
+    def remove(self, key: int) -> None:
+        bucket = self.buckets[self._hash(key)]
+        if key in bucket:
+            bucket.remove(key)
 
+    def contains(self, key: int) -> bool:
+        bucket = self.buckets[self._hash(key)]
+        return key in bucket
 
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
