d2a5aedb5a2db94a1c7e87468289b788f42daaf9
GitHub Action <action@github.com>
2025-06-05 05:29:41 +0000

    [LeetCode Sync] Runtime - 16 ms (33.88%), Memory - 51.7 MB (80.46%)

---
+++ action-sync/0816-design-hashset/solution.java
@@ -0,0 +1,42 @@
+class MyHashSet {
+    private final int SIZE = 1000;
+    private LinkedList<Integer>[] buckets;
+
+    public MyHashSet() {
+        buckets = new LinkedList[SIZE];
+        for (int i =0; i<SIZE; i++){
+            buckets[i] = new LinkedList<>();
+        }
+        
+    }
+    private int hash (int key){
+        return key%SIZE;
+    }
+    
+    public void add(int key) {
+        int index = hash(key);
+        if(!buckets[index].contains(key)){
+            buckets[index].add(key);
+        }
+        
+    }
+    
+    public void remove(int key) {
+        int index = hash(key);
+        buckets[index].removeFirstOccurrence(key);
+        
+    }
+    
+    public boolean contains(int key) {
+        int index = hash(key);
+        return buckets[index].contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
