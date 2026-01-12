f383d97a0091798804d29c82785558d50113cd39
GitHub Action <action@github.com>
2026-01-12 19:45:51 +0000

    [LeetCode Sync] Runtime - 788 ms (31.30%), Memory - 18.3 MB (77.38%)

--- action-sync/0015-3sum/solution.py
+++ action-sync/0015-3sum/solution.py
@@ -1,34 +1,33 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # 第一步：排序
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
         res = []
-
+        
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # 跳过重复的第一个数
-
-            left, right = i + 1, len(nums) - 1
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left, right = i+1, len(nums)-1
 
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
+                total = nums[i] + nums[left] + nums[right]                                               # -2, -2, -1, ..., 3, 4
 
                 if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-
-                    # 跳过重复的第二个数
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    # 跳过重复的第三个数
-                    while left < right and nums[right] == nums[right - 1]:
+                    res.append([nums[i] , nums[left] , nums[right]])
+                    while left < right and nums[right] == nums[right-1]:
                         right -= 1
-
+                    while left <  right and nums[left] == nums[left+1]:
+                        left += 1
                     left += 1
                     right -= 1
 
-                elif total < 0:
-                    left += 1  # 总和太小，往右边找更大的数
+                elif total > 0:
+                    right -= 1
                 else:
-                    right -= 1  # 总和太大，往左边找更小的数
+
+                    left += 1
 
         return res
-
