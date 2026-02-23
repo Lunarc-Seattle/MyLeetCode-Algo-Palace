b299002bc4f6a76718ce88ecfc53b12a1c126356
JIE （FIA） YIN <66171272+Lunarc-Seattle@users.noreply.github.com>
2026-02-22 16:39:42 -0800

    Time: 0 ms (100.00%) | Memory: 19.8 MB (11.28%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -8,6 +8,7 @@ class Solution:
             mid = left + (right-left)//2
             #在二分查找中使用 (right - left) // 2 而不是直接 (left + right) // 2，最核心的原因是为了防止“溢出” (Overflow)，
             mid_val = matrix[mid//n][mid%n]
+            #象你在排队，每排站 n 个人。要算出你在当前排的第几个，你应该用总人数除以每排的人数 n 取余数，而不是除以总共有多少排 m
               #n 是列数！
             if mid_val == target:
                 return True
