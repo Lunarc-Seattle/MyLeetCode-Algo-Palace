0d507887b60fa4c7f5b912496f5f8c60fa578d00
JIE （FIA） YIN <66171272+Lunarc-Seattle@users.noreply.github.com>
2026-02-27 22:57:29 -0800

    Time: 159 ms (82.16%) | Memory: 20.6 MB (86.17%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -8,9 +8,13 @@ class Solution:
             total = 0
             for p in piles:
                 total = total + (p+mid-1)//mid
+                #这里是重点 p//mid 往上加一个就是（p+mid-1）//mid
+                # (p + mid - 1) // mid 就是程序员版的“向上取整”函数
+                # 在 Python 中，// 符号是“地板除”（向下取整）。
             
             if total <= h:
                 high = mid
+                #如果会排出正确答案的话，就不能mid-1
             else:
                 low = mid+1
         return low
