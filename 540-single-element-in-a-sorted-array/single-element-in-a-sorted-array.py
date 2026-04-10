8d16cf2db6a6f8f3971cb66f2bf47b7274b46222
JIE （FIA） YIN <66171272+Lunarc-Seattle@users.noreply.github.com>
2026-04-10 13:19:05 -0700

    Time: 0 ms (100.00%) | Memory: 27.2 MB (18.24%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,17 +1,16 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        left, right = 0, len(nums)-1
-        while left < right:
-            mid = (left+ right)//2
+        if len(nums) == 1:
+            return nums[0]
+        left , right = 0 , len(nums)-1
+        while left< right:
+            mid = (left+right)//2
             if mid%2 == 1:
-                mid -= 1
-                #我们需要让 mid 指向 一对元素的“第一个位置”
+                mid = mid-1
+
             if nums[mid] == nums[mid+1]:
-                # break point is in the right
                 left = mid+2
             else:
-                # 断了 → 答案在左边（包括 mid）
                 right = mid
-        return nums[right]
-        #最后right = left
+        return nums[left]
         
