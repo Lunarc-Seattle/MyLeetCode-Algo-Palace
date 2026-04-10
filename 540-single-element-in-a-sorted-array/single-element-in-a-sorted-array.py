5810bddd888063a42129fa5048440831f0c33d47
JIE （FIA） YIN <66171272+Lunarc-Seattle@users.noreply.github.com>
2026-04-10 11:07:13 -0700

    Time: 0 ms (100.00%) | Memory: 27 MB (72.25%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -5,11 +5,13 @@ class Solution:
             mid = (left+ right)//2
             if mid%2 == 1:
                 mid -= 1
+                #我们需要让 mid 指向 一对元素的“第一个位置”
             if nums[mid] == nums[mid+1]:
                 # break point is in the right
                 left = mid+2
             else:
                 # 断了 → 答案在左边（包括 mid）
                 right = mid
-        return nums[left]
+        return nums[right]
+        #最后right = left
         
