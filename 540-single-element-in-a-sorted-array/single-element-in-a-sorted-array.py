b1f8e86628fb11c2dc79e0eedbba5eda3571c6bd
JIE （FIA） YIN <66171272+Lunarc-Seattle@users.noreply.github.com>
2026-04-10 13:20:22 -0700

    Time: 0 ms (100.00%) | Memory: 26.9 MB (94.25%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,16 +1,15 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        left , right = 0 , len(nums)-1
-        while left< right:
-            mid = (left+right)//2
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left+ right)//2
             if mid%2 == 1:
-                mid = mid-1
-
+                mid -= 1
             if nums[mid] == nums[mid+1]:
+                # break point is in the right
                 left = mid+2
             else:
+                # 断了 → 答案在左边（包括 mid）
                 right = mid
         return nums[left]
         
