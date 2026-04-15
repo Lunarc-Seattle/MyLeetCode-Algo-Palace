949dcc37c4e2144f52062ffdfb5ade9737616da6
GitHub Action <action@github.com>
2026-04-15 05:02:01 +0000

    [LeetCode Sync] Runtime - 4 ms (30.84%), Memory - 19.3 MB (79.90%)

--- action-sync/0747-min-cost-climbing-stairs/solution.py
+++ action-sync/0747-min-cost-climbing-stairs/solution.py
@@ -1,11 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = [0]*(n+1)
-    
+        prev2, prev1 = 0 , 0 
+        #prev2 = dp[i-2]
+        # prev1 = dp[i-1]
+        # prev2， prev1， curr
+        for i in range(2,len(cost)+1):
+            curr = min(
+                prev1+cost[i-1],
+                prev2+cost[i-2]
+            )
+            prev2 = prev1 # prev2 ← prev1
+            prev1 = curr
+        return prev1
+        #prev1 = 最终的 dp[n]
 
-        for i in range(2, n+1):
-            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
-        
-        return dp[n]
-        
+
