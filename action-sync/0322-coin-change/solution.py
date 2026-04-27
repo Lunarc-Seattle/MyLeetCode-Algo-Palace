312937892e2ab869cc8b240a537bc981b220abbb
GitHub Action <action@github.com>
2026-04-27 18:10:44 +0000

    [LeetCode Sync] Runtime - 436 ms (94.13%), Memory - 19.5 MB (77.62%)

--- action-sync/0322-coin-change/solution.py
+++ action-sync/0322-coin-change/solution.py
@@ -1,135 +1,23 @@
-############################################################
-# L0322.py
-# Author: Jagadeesh Vasudevamurthy
-# Copyright: Jagadeesh Vasudevamurthy 2020
-###########################################################
-
-############################################################
-# All imports
-###########################################################
-from typing import List
-from time import process_time 
-
-
 class Solution:
-    ## YOU CANNOT CHANGE THIS INTERFACE
-    ## LEETCODE INTERFACE
     def coinChange(self, coins: List[int], amount: int) -> int:
-        ## YOU CANNOT CHANGE ANYTHING IN THIS PROCEDURE
-        work = [0]
-        changes = [] #If change cannot be given, you must put -1 in changes[0]
-        show = False
-        p = L0322(coins,amount,changes,work,show)
-        num_change = len(changes)
-        if (num_change == 1):
-            if (changes[0] == -1):
-                num_change = -1
-        return num_change
-
-class L0322:
-    def __init__(self, coins: List[int], amount:'int', changes:'list of int', work:'List of size 1',show:'boolean'):
-        #NOTHING CAN BE CHANGED
-        self._d = coins
-        self._n = amount
-        self._ans = changes
-        self._work = work
-        self._show = show
-        # YOU MUST GENERATE V table and k table
-        self._v = [] 
-        self._k = []
-        # You can have any number of data structures here
-        # MUST WRITE TWO ROUTINES
-        self._alg()
-        self._get_solution() 
-
-    def _increment_work(self):
-        self._work[0] = self._work[0] + 1
-
-
-    ############################################################
-    # WRITE CODE BELOW
-    ###########################################################
-
-    ############################################################
-    # TIME (n * k ) = O(n)
-    # SPACE O(n)
-    ############################################################
-    def _alg(self):
-        n = self._n
-        coins = self._d
-
-        # Initialize arrays to store minimum number of coins needed
-        INF = float('inf')
-        dp = [INF] * (n + 1)
+        #因为我们要一个“不可能的最大值”，方便后面取最小
+        dp = [amount+1]*(amount+1)
         dp[0] = 0
 
-        k = [0] * (n + 1)
-        # Iterate through all amounts from 1 to n
-        for i in range(1, n + 1):
-            min_coin = INF
-            for coin in coins:
-                self._work[0] += 1
-                if i - coin >= 0 and dp[i - coin] + 1 < dp[i]:
-                    dp[i] = min(dp[i], dp[i - coin] + 1)
-                    k[i] = coin  # Update k[i] with the coin denomination that minimizes the number of coins for amount i
-                    
-
-        self._v = dp
-        self._k = k
-
-        # Generate ans table to trace back the solution
-        if self._v[n] != float('inf'):
-           
-            while n > 0:
-                coin = self._k[n]
-                self._ans.append(coin)
-                n -= coin
-        else:
-            self._ans.append(-1) 
-        
-                
-                
-
-
-    ############################################################
-    # NOTHING CAN BE CHANGED IN THIS ROUTINE BELOW
-    ###########################################################
-    def _get_solution(self):
-        if (self._show):
-            a = []
-            for i in range(self._n + 1):
-                a.append(i)
-            print(a)
-            print(self._v)
-            print(self._k)
-        if (self._n < 1000):
-            for i in range(self._n + 1):
-                if (self._show):
-                    print("minimum change for",i,"cents can be achieved using",self._v[i],"coins.")
-                self._get_solution1(i)
-        else:
-            self._get_solution1(self._n)
+        for i in range (1, amount+1):
+            for c in coins:
+                if i>= c:#如果i=1，coin只有2的话就太大了，凑不了
+                #“沉默规则”。如果手里的硬币都太大，if i >= coin 这个条件就永远不会成立。既然进不去这个 if 内部，那 dp[i] 就会停留在它出生时的样子。
+                    dp[i] = min(dp[i],dp[i-c]+1)
+                #为什么+1？
+                #因为从最后一枚硬币往前推，是旧的方案
+                #+1 是旧的方案 + 一枚新硬币
+                #当你计算 dp[i - coin] 时，你其实是在翻看你的历史记录本。比如你想凑 7块钱，手里抓着一枚 2块钱 的硬币。你往前看：“凑齐 5 块钱最少需要几枚？”
+                #记事本告诉你：dp[5] = 1（也就是那一枚 5 块钱硬币）。这个 1 就是你说的 “旧方案”。
+                #2. 为什么“+1”？这一步就是动作的执行。
+                #你现在决定把手里这枚 2块钱 投进储钱罐。动作：你增加了一枚硬币。数学表达：就是在旧方案的枚数上 +1。
+        if dp[amount] == amount+1:
+            return -1
+        return dp[amount]
             
-    ############################################################
-    # TIME O(n)
-    # SPACE THETA(1)
-    # How will you give change for p cents
-    # WRITE CODE BELOW
-    ############################################################
-    def _get_solution1(self,p:'int'):
-        
-        if self._v[p] != float('inf'):
-            amount = p
-            given = 0
-            i=1
-            while amount > 0:
-                coin = self._k[amount]
-                given += coin
-                print(
-                    f"{i} : Give coin {coin}. So far you have given= {given}. Remaining to give {amount - coin}")
-                amount -= coin
-                i += 1
-
-        else:
-            print(f"{p} : No optimal solution found")
         
