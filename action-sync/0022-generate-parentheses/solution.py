85797de10e2d05ec2609f5682bb5859c68dec5ab
GitHub Action <action@github.com>
2026-05-05 20:19:30 +0000

    [LeetCode Sync] Runtime - 3 ms (30.45%), Memory - 19.4 MB (74.19%)

--- action-sync/0022-generate-parentheses/solution.py
+++ action-sync/0022-generate-parentheses/solution.py
@@ -1,15 +1,27 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def backtrack(s, open_count, close_count):
-            if len(s) == 2 * n:
-                result.append(s)
-                return
-            if open_count < n:
-                backtrack(s + "(", open_count + 1, close_count)
-            if close_count < open_count:
-                backtrack(s + ")", open_count, close_count + 1)
+        res = []
         
-        result = []
-        backtrack("", 0, 0)
-        return result
+        def backtrack (path, open, close ):
+            if len(path) == 2*n:
+                res.append("".join(path))
+                return
+
+            #一开始必须先放左括号 
+            if  open < n:
+                path.append('(')
+                backtrack(path, open+1 , close)
+                path.pop()
+
+            if close<open:
+                path.append(')')
+                backtrack(path, open , close+1)
+
+                path.pop()
+                # 会一直退到 open = 3 close = 0这一层
+                # 然后也没有新选择了，继续退。"(((" 退到 "((" -- open = 2 close = 0这一层
+                #因为这个 open = 2 的这一层，第一个 if 早就执行过了
+                # 现在是从第一个 if 的递归里回来，回来之后代码位置已经走到第一个 if 后面了，所以接着执行第二个 if。
+        backtrack([], 0, 0)
+        return res
         
