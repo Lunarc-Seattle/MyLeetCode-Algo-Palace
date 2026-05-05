9b53b0b8b359cde53520b33cc02adb86c9236d42
GitHub Action <action@github.com>
2026-05-05 02:01:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (34.52%)

--- action-sync/0046-permutations/solution.py
+++ action-sync/0046-permutations/solution.py
@@ -1,16 +1,26 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return [ [ ] ]
-        res = [ ] # !!!attention 
+        res = []
+        path = []
+        used = [False] * len(nums)
 
-        perms = self.permute(nums[1:]) # [[]], [[], [3]], [[], [2], [2,3], [3,2]], [[],]
-        for perm in perms:
-            for i in range(len(perm)+1):
-                temp = perm.copy()
-                temp.insert(i, nums[0])
-                res.append(temp)
+        def back ():
+            if len(path) == len(nums):
+                res.append(path[:])# !
+                return
 
-        return res  
+            #为什么排列不需要 start？因为排列里：[1, 2, 3]和：[2, 1, 3]是不一样的答案。
+            
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                path.append(nums[i])
+                used[i] = True
+                
+                back()
+                path.pop()
+                used[i] = False
+        back()
+        return res
+            
         
-
